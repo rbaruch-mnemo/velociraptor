dc84c7d16420276cc75a01db4d25196353fe06df
Rodolfo Baruch Guerra <r.baruchguerra@mnemo.mx>
2022-10-05 18:15:48 -0500

    cambios, pendiente creación del archivo .msi para windows

--- Generar-ArchivosDeConfiguracion.ps1
+++ Generar-ArchivosDeConfiguracion.ps1
@@ -6,7 +6,8 @@ Param(
     [Parameter(Mandatory=$true, HelpMessage="Ingresa el/los servidores  DNS para la configuración de red`nPE: '8.8.8.8,1.1.1.1'")] [string] $DNS
 )
 
-function Validar-IPv4($direccionIP) {
+function Validar-IPv4 { 
+    param ($direccionIP)
     $valid_ipv4 = "^([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$"
     if ($direccionIP -match $valid_ipv4) { return $direccionIP } else {
         Write-Host "[-] ERROR al validar dirección IP ingresada en el campp {IP/Gateway/DNS}.`nRevisar los valores ingresados e intentar de nuevo."
@@ -14,9 +15,10 @@ function Validar-IPv4($direccionIP) {
     }
 }
 
-function Validar-RutaTrabajo($DirectorioTrabajo) {
-    $DirectorioTrabajo = Convert-Path $DirectorioTrabajo # Para casos donde sea "." o ".."
+function Validar-RutaTrabajo { 
+    param ($DirectorioTrabajo)
     if (Test-Path -Path $DirectorioTrabajo) {
+        $DirectorioTrabajo = Convert-Path $DirectorioTrabajo # Para casos donde sea "." o ".."
         return $DirectorioTrabajo
     } else {
         $ficheroAnterior = Split-Path -Path $DirectorioTrabajo
@@ -27,7 +29,7 @@ function Validar-RutaTrabajo($DirectorioTrabajo) {
                 New-Item -Path $DirectorioTrabajo -ItemType "Directory"
                 Write-Host "Se ha creado la carpeta $leaf.`nRaiz del ambiente: $DirectorioTrabajo"
                 return $DirectorioTrabajo
-            }
+            } else {exit}
         } else {
             Write-Host "[-] ERROR en el path ingresado como directorio de salida."
             exit
@@ -35,26 +37,39 @@ function Validar-RutaTrabajo($DirectorioTrabajo) {
     }
 }
 
-function Instalar-Requisitos() {
-}
-
-function Instalar-Requisitos() {
+function Verificar-Requisitos() {
+    Write-Host "[+] Verificando requisitos de Velociraptor..."
     if ((Get-WindowsCapability -Online -Name NetFx3~~~~).State -ne "Installed") {
-        Write-Host -NoNewLine "[*] Instalando .NET 3.5.1..."
+        Write-Host -NoNewLine "`t[*] Instalando .NET 3.5.1..."
         DISM /online /Enable-Feature /FeatureName:"NetFx3" | Out-Null
         Enable-WindowsOptionalFeature -Online -FeatureName "NetFx3" | Out-Null
         Write-Host -Fore Green "Listo!"
-    } else { Write-Host "[*] Ya se encuentra instalado .NET 3.5.1." }
+    } else { Write-Host "`t[*] Ya se encuentra instalado .NET 3.5.1." }
     if (-not (Test-Path -Path "C:\Program Files (x86)\WiX Toolset v3.11)")) {
-        Write-Host -NoNewLine "[*] Instalando utilierias de WIX para crear clientes..."
-        Start-Process -Wait -FilePath ".\bins\wix311.exe" -ArgumentList "/S" -PassThru
+        Write-Host -NoNewLine "`t[*] Instalando utilierias de WIX para crear clientes..."
+        Start-Process -Wait -FilePath ".\bins\wix311.exe" -ArgumentList "/S" -PassThru | Out-Null
         Write-Host -Fore Green "Listo!"
-    } else { Write-Host "[*] Ya se encuentran instaladas las utilerías de WIX." }
+    } else { Write-Host "`t[*] Ya se encuentran instaladas las utilerías de WIX." }
 }
 
-function Generar-ArchivosConfiguración($DirectorioTrabajo, $IP, $Mascara, $Gateway, $DNS) {
-    #Write-Host ":)"
-    Instalar-Requisitos
+function Generar-ArchivosConfiguracion($DirectorioTrabajo, $IP, $Mascara, $Gateway, $DNS) {
+    Write-Host "[+] Generando carpetas dentro de la ruta de trabajo..."
+    "servidor","clientesWindows","clientesLinux" | % {mkdir $DirectorioTrabajo\$_} | Out-Null
+
+    # Servidor Velociraptor
+    Write-Host "`t[+] Generando archivos de configuración para el servidor..."
+    Copy-Item -Path ".\archivosBase\server.config.yaml" -Destination "$DirectorioTrabajo\servidor" -Force | Out-Null
+    (Get-Content "$DirectorioTrabajo\servidor\server.config.yaml").replace('{{IP}}', $IP) | Set-Content "$DirectorioTrabajo\servidor\server.config.yaml"
+
+    # Clientes Windows
+    Write-Host "`t[+] Generando archivos de configuración para los clientes Windows..."
+    Copy-Item -Path ".\archivosBase\client.config.yaml" -Destination ".\bins\wix_orig\output" -Force | Out-Null
+    (Get-Content ".\bins\wix_orig\output\client.config.yaml").replace('{{servidor}}', $IP) | Set-Content ".\bins\wix_orig\output\client.config.yaml"
+    "Iniciando BAT..."
+    Start-Process "cmd.exe" "/c .\bins\wix_orig\build_custom.bat"
+    #Move-Item -Path ".\bins\wix_orig\custom.msi" -Destination "$DirectorioTrabajo\clientesWindows" -Force
+    #Remove-Item -Path ".\bins\wix_orig\output\client.config.yaml"
+    #msiexec /i custom.msi
 }
 
 # Validaciones de valores ingresados
@@ -63,6 +78,11 @@ $IP = Validar-IPv4($IP)
 $Gateway = Validar-IPv4($Gateway)
 $DNS = Validar-IPv4($DNS)
 if ($Mascara -NotMatch "^[0-9]{2}$") { Write-Host "[-] ERROR al validar la máscara ingresada.`nIngresar el número de octetos de la máscara. P.E: 24."; exit }
-
-# Creación de archivos con los valores validados
-Generar-ArchivosConfiguración($DirectorioTrabajo, $IP, $Mascara, $Gateway, $DNS)
+#Start
+Verificar-Requisitos
+if (($DirectorioTrabajo -and $IP -and $Mascara -and $Gateway -and $DNS) -ne $null) {
+    Write-Host "DT: $DirectorioTrabajo`nIP: $IP`nMASK: $Mascara`nGW: $Gateway`nDNS: $DNS`n"
+    Generar-ArchivosConfiguracion -DirectorioTrabajo $DirectorioTrabajo -IP $IP -Mascara $Mascara -Gateway $Gateway -DNS $DNS 
+} else {
+    Write-Host "[-] ERROR: Ingrese valores para IP, Mascara, Gateway y DNS."; exit
+}
--- server_start.sh
+++ server_start.sh
@@ -1,5 +1,2 @@
-
-
-
 ./velociraptor-v0.6.6-1-linux-amd64 --config server.config.yaml debian server --binary velociraptor-v0.6.6-1-linux-amd64 # Creación de binario para servidor
 dpkg -i velociraptor-v0.6.6-1-linux-amd64.deb # Iniciar el servicio de velociraptor
