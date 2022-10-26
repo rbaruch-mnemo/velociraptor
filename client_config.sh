5bc5191e2951386acd3fa76ea8bfcd13242bea44
rbaruch-mnemo <105951982+rbaruch-mnemo@users.noreply.github.com>
2022-10-26 14:50:54 -0500

    Delete client_config.sh

--- client_config.sh
+++
@@ -1,3 +0,0 @@
-#wget https://github.com/Velocidex/velociraptor/releases/download/v0.6.6-1/velociraptor-v0.6.6-1-linux-amd64
-chmod +x velociraptor-v0.6.6-1-linux-amd64
-./velociraptor-v0.6.6-1-linux-amd64 --config client.config.yaml -v
